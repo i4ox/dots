be85fe9ad6cc25f3cd176f714be6c0590a1d41c9
asa <arthurlokhov@gmail.com>
2024-10-31 09:13:15 +0300

    add: add lint support

--- .config/nvim/lua/plugins/lsp/lint.lua
+++ .config/nvim/lua/plugins/lsp/lint.lua
@@ -1,1 +1,13 @@
-return {}
+return {
+    "mfussenegger/nvim-lint",
+    config = function()
+        require("lint").linters_by_ft = {
+            lua = { 'luacheck' },
+        }
+        vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+            callback = function()
+                require('lint').try_lint()
+            end,
+        })
+    end,
+}
